923baf7aee803b7dea31490a402e2cdb7622aeb1
Joelmir Uchoa <joelmiruchoa@gmail.com>
2024-12-03 09:23:22 -0300

    removendo pastas que não são utilizados iniciando o banco de dados

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -6,8 +6,5 @@ use Illuminate\Http\Request;
 
 class GuestController extends Controller
 {
-    public function create()
-    {
-        return view('auth.guest-create'); 
-    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,27 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GuestController;
+use App\Http\Controllers\BrotherController; //rota para buscar os dados pelo número SIM
 
+
+// Rota inicial redireciona para 'select-user'
 Route::get('/', function () {
-    return redirect('/login');
-});
-
-// Para o painel de usuário
-Route::get('/dashboard', function () {
-    return view('user-main');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-// Para selecionar o tipo de usuário
-Route::middleware('auth')->get('/select-user', function () {
-    return view('auth.select-user');
+    return redirect('select-user');
 })->name('select-user');
 
-// Para a criação de convidado
-// Route::get('/guest/create', [GuestController::class, 'create'])->name('guest.create');
-Route::middleware('auth')->get('/guest/create', [GuestController::class, 'create'])->name('guest.create');
+// Rota para a página de seleção de usuário
+Route::get('/select-user', function () {
+    return view('select-user');
+})->name('select-user');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+//rota para buscar os dados pelo número SIM
+Route::post('/brother-data', [BrotherController::class, 'getBrotherData']);
 
-require __DIR__.'/auth.php';
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
